1394c55acd9f39330bd61232977d5320b3ed6a17
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2018-07-20 16:51:12 +0200

    index.html args: width -> w, height -> h

--- screenshot.sh
+++ screenshot.sh
@@ -1,4 +1,4 @@
 chrome="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 firefox="/Applications/Firefox.app/Contents/MacOS/firefox"
-"${!1}" --headless --screenshot --window-size=$2,$3 "file://$(pwd)/index.html?width=$2&height=$3"
+"${!1}" --headless --screenshot --window-size=$2,$3 "file://$(pwd)/index.html?w=$2&h=$3"
 pngtopam screenshot.png
