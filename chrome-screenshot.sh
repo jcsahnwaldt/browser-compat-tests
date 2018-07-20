cbfd11cb55ac61b7b618c1cb03b0ffe62368a836
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2018-07-20 16:38:26 +0200

    *-screenshot.sh: use $(pwd)/index.html instead of copy to /tmp

--- chrome-screenshot.sh
+++ chrome-screenshot.sh
@@ -1,3 +1,2 @@
-cp index.html /tmp
-"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --headless --screenshot --window-size=$1,$2 "file:///tmp/index.html?width=$1&height=$2"
+"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --headless --screenshot --window-size=$1,$2 "file://$(pwd)/index.html?width=$1&height=$2"
 pngtopam screenshot.png
