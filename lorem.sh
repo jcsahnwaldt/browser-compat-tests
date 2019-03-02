ea66af65bf9397d1a8567af70d56975d960b7035
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2019-03-02 12:47:07 +0100

    index.html -> lorem.html, screenshot.sh -> lorem.sh

--- lorem.sh
+++ lorem.sh
@@ -1,4 +1,5 @@
+if [ -z "$1" ] ; then echo usage: "$0" '<chrome|firefox>'; exit 1; fi
 chrome="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 firefox="/Applications/Firefox.app/Contents/MacOS/firefox"
-"${!1}" --headless --screenshot --window-size=$2,$3 "file://$(pwd)/index.html?w=$2&h=$3&f=$4"
+"${!1}" --headless --screenshot --window-size=10,15 "file://$(pwd)/lorem.html?w=10&h=15&f=%20"
 pngtopam screenshot.png | tail -n +4
