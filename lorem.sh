8236dd63336ad6bbac14188565825cf238560afe
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2019-03-02 13:04:54 +0100

    lorem.html, lorem.sh: refactoring, handle input text

--- lorem.sh
+++ lorem.sh
@@ -1,5 +1,21 @@
+#!/bin/sh
+
+set -e
+
 if [ -z "$1" ] ; then echo usage: "$0" '<chrome|firefox>'; exit 1; fi
+
 chrome="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 firefox="/Applications/Firefox.app/Contents/MacOS/firefox"
-"${!1}" --headless --screenshot --window-size=10,15 "file://$(pwd)/lorem.html?w=10&h=15&f=%20"
+
+width=${2:-15}
+height=${3:-10}
+text="$4"
+fill=${5:-%20}
+
+url="file://$(pwd)/lorem.html?width=$width&height=$height&text=$text&fill=$fill"
+
+rm -f screenshot.png
+
+"${!1}" --headless --screenshot --window-size="$width,$height" "$url"
+
 pngtopam screenshot.png | tail -n +4
