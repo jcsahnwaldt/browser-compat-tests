d77015db761dacfbd7af8832dc8d794ab9fa5825
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2019-03-02 12:32:50 +0100

    screenshot.sh: drop PAM header lines

--- screenshot.sh
+++ screenshot.sh
@@ -1,4 +1,4 @@
 chrome="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 firefox="/Applications/Firefox.app/Contents/MacOS/firefox"
 "${!1}" --headless --screenshot --window-size=$2,$3 "file://$(pwd)/index.html?w=$2&h=$3&f=$4"
-pngtopam screenshot.png
+pngtopam screenshot.png | tail -n +4
